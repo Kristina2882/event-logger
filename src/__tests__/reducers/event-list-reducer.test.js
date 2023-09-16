63c852b3bd2840928ea15cb4b64120221bc657a5
Khrystyna <k-r-i-s-t-i-n-a@i.ua>
2023-09-16 18:07:22 +0300

    CRUD tested.

--- src/__tests__/reducers/event-list-reducer.test.js
+++ src/__tests__/reducers/event-list-reducer.test.js
@@ -1,8 +1,100 @@
 import eventListReducer from "../../reducers/event-list-reducer";
 
 describe('eventListReducer', () => {
+
+let action;
+const newEventData = {
+    name: 'Item1',
+    description: 'I was getting the same warning',
+    numberOfEvent: 0,
+    id: 1
+};
+const updateEventData = {
+    name: 'Item1UPD',
+    description: 'I was getting the same warning again',
+    numberOfEvent: 0,
+    id: 1
+};
+
+const currentState = {
+    1: {
+        name: 'Item1',
+    description: 'I was getting the same warning',
+    numberOfEvent: 0,
+    id: 1
+    },
+    2:  {
+        name: 'Item2',
+    description: 'You was getting the same warning',
+    numberOfEvent: 0,
+    id: 2
+    }
+}
+
+
     test ('should return default state if no action is passed into reducer', () => {
         expect(eventListReducer({}, {type: null})).toEqual({});
 
     });
+    test ('should add the new event to the eventList', () => {
+        const {name, description, numberOfEvent, id} = newEventData;
+        action = {
+            type: 'ADD_EVENT',
+            name: name,
+            description: description,
+            numberOfEvent:numberOfEvent,
+            id: id
+        }
+        expect(eventListReducer({}, action)).toEqual({
+            [id] : {
+                name:name,
+                description: description,
+                numberOfEvent: numberOfEvent,
+                id: id
+
+            }
+    });
+
+    });
+
+    test('should update the event', () => {
+        const {name, description, numberOfEvent, id} = updateEventData;
+        action = {
+            type: 'ADD_EVENT',
+            name: name,
+            description: description,
+            numberOfEvent:numberOfEvent,
+            id: id
+        }
+        expect(eventListReducer(currentState, action)).toEqual({
+            [id] : {
+                name:name,
+                description: description,
+                numberOfEvent: numberOfEvent,
+                id: id
+            },
+            2:  {
+                name: 'Item2',
+            description: 'You was getting the same warning',
+            numberOfEvent: 0,
+            id: 2
+            }
+    });
+});
+
+    test ('should delete the event',() => {
+        action = {
+            type: 'DELETE_EVENT',
+            id: 2
+        }
+        expect(eventListReducer(currentState, action)).toEqual({
+          1: {
+            name: 'Item1',
+            description: 'I was getting the same warning',
+            numberOfEvent: 0,
+            id: 1
+          }
+        })
+    });
+
 });
--- src/reducers/event-list-reducer.js
+++ src/reducers/event-list-reducer.js
@@ -1,5 +1,23 @@
 const reducer = (state = {}, action) => {
+  const {name, description, numberOfEvent, id} = action;
+  switch (action.type) {
+ case('ADD_EVENT'):
+ return Object.assign({}, state, {
+    [id] : {
+        name: name,
+        description: description,
+        numberOfEvent: numberOfEvent,
+        id: id
+    }
+ });
+ case ('DELETE_EVENT'):
+    let newState = {...state};
+    delete newState[id];
+    return newState;
+
+ default:   
  return state;
+  }
 }
 
 export default reducer;
